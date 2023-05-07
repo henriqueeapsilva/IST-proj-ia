b1d5e680344ab46c65a87a8d50695f33657670ae
Afonso Azaruja <afonso.azaruja@mailbox.org>
2023-05-07 12:12:59 +0100

    Edited names

--- projP42223base/bimaru.py
+++ projP42223base/bimaru.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 127:
+# 103606 Henrique Silva
+# 103624 Afonso Azaruja
 
 import sys
 from search import (
